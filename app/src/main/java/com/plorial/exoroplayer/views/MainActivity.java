564c5de461114eae63632c24f0bf5b2428857833
Plorial <lanshunoda@rambler.ru>
2016-06-26 18:43:43 +0300

    added runtime permissions

--- app/src/main/java/com/plorial/exoroplayer/views/MainActivity.java
+++ app/src/main/java/com/plorial/exoroplayer/views/MainActivity.java
@@ -1,10 +1,21 @@
 package com.plorial.exoroplayer.views;
 
+import android.Manifest;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.DialogOnAnyDeniedMultiplePermissionsListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.plorial.exoroplayer.R;
 import com.plorial.exoroplayer.model.Item;
 
@@ -23,9 +34,25 @@ public class MainActivity extends AppCompatActivity implements FileChooseFragmen
             FragmentTransaction transaction = getFragmentManager().beginTransaction();
             transaction.add(R.id.fragment_container, fileExplorerFragment);
             transaction.commit();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                checkPermissions();
+            }
         }else {
             fileExplorerBundle = savedInstanceState.getBundle(this.getClass().getSimpleName());
         }
+    }
+
+    private void checkPermissions() {
+        MultiplePermissionsListener dialogMultiplePermissionsListener =
+                DialogOnAnyDeniedMultiplePermissionsListener.Builder
+                        .withContext(getApplicationContext())
+                        .withTitle("Read External Storage & Internet permissions")
+                        .withMessage("That permission are needed to open video from internet and local storage")
+                        .withButtonText(android.R.string.ok)
+                        .withIcon(R.mipmap.ic_launcher)
+                        .build();
+        Dexter.checkPermissions(dialogMultiplePermissionsListener, Manifest.permission.INTERNET, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.READ_EXTERNAL_STORAGE);
+        Dexter.continuePendingRequestsIfPossible(dialogMultiplePermissionsListener);
     }
 
     @Override
